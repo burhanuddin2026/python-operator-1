f33f9f75bbbf420187277ef8dd5e3d78877f8908
burhanuddin2026 <sarahdubaiwala90@gmail.com>
2026-05-30 17:39:19 +0530

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,10 @@
+print("enter marks obtainedin 4 subjects")
+maths=int(input("maths:"))
+english=int(input("english:"))
+science=int(input("science:"))
+hindi=int(input("hindi:"))
+sum=maths+english+science+hindi
+print("sum of maths,english,science and hindi is:",sum)
+perc=(sum/400)*100
+print(end="percentage mark=")
+print(perc)
